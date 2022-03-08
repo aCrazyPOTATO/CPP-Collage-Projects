810ee85b96238f41fdafd541936ff833380915bd
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-03-08 21:02:48 +0545

    Pointer Updated

--- Pointer.cpp
+++ Pointer.cpp
@@ -11,5 +11,11 @@ int main()
     cout << "The Address of i is" << b << endl;
     // value
     cout << "The Value at address b is " << *b << endl;
+    // pointer address to pointer
+    int **c = &b;
+    cout << "The Address of b is" << &b << endl;
+    cout << "The Address of b is" << c << endl;
+    cout << "The  value at Address c  is" << *c << endl;
+    cout << "The value at address value_at(value_at(c))  is " << **c << endl;
     return 0;
 }
