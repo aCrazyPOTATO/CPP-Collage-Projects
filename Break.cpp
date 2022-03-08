b241b194b054914767b670558dd29148c9e95489
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-03-08 20:42:19 +0545

    Added Break Statement

---
+++ Break.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i = 0; i < 100; i++)
+    {
+        cout << i << endl;
+        if (i == 69)
+        {
+            break;
+        }
+    }
+}
