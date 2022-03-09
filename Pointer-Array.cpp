6b9ece8952236f3a5ebb66194d29c2b7752c3307
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-03-09 21:00:01 +0545

    Added

--- Pointer-Array.cpp
+++ Pointer-Array.cpp
@@ -5,8 +5,10 @@ int main()
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
     int *ptr;
     ptr = arr;
-    for (i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        cout <<
+        cout << "Value of Array[" << i << "]=" << *ptr << endl;
+        ptr++;
     }
+    return 0;
 }
