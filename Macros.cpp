a159a69d276d0c6a1138e1a7f885245143e74b2f
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-03-09 21:31:15 +0545

    Macros

---
+++ Macros.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define mul(a, b) a *b;
+#define div(a, b) a / b;
+int main()
+{
+    int x = 2, y = 3;
+    float n = 5.0, m = 2.0;
+    cout << mul(x, y);
+    cout << "\n";
+    cout << div(n, m);
+    return 0;
+}
