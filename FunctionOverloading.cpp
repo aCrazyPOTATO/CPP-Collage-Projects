cb57fca1d77820cbb6e88a3b84f2cd3617fdc6f9
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-03-11 21:18:32 +0545

    Done

---
+++ FunctionOverloading.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int mul(int a, int b)
+{
+    return (a * a);
+}
+float mul(float a, float b)
+{
+    return (a * b);
+}
+int main()
+{
+    int x, y;
+    float m, n;
+    cout << "Enter two Integers:" << endl;
+    cin >> x >> y;
+    cout << "Enter Two Floats:" << endl;
+    cin >> m >> n;
+    cout << "The Product of Integers:" << mul(x, y);
+    cout << "\n";
+    cout << "The Poduct of Floats:" << mul(m, n);
+    return 0;
+}
