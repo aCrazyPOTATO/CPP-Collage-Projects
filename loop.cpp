b7331de57c8f536bb2f60ae8e5590c19223dc644
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-03-23 17:22:37 +0545

    Create loop.cpp

---
+++ loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j = 150, k;
+    for (i = 0; i <= j; i++)
+    {
+        cout << This is << i << th time of Output << endl;
+    }
+}
