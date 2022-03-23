2bf856b1561a31f6026013937e77a2a8d3b3ad84
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-03-23 17:26:09 +0545

    Create addfloats.cpp

---
+++ addfloats.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, s;
+    cout << "Enter Two Numbers: \n"
+         << endl;
+    cin >> a >> b;
+    s = a + b;
+    cout << "Sum of Two Numbers is:" << s << endl;
+}
