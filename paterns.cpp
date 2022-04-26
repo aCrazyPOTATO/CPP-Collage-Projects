b712614634f38d0bdb56e143b462f3b42eccc03f
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-04-26 17:46:05 +0545

    I didnot make it 😊

--- paterns.cpp
+++ paterns.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
 int main()
 {
+    int i, j;
     int row, col;
     cout << "Enter Row and columns" << endl;
     cin >> row >> col;
-    for (int i = 1; i <= row; i++)
+    for (i = 1; i <= row; i++)
     {
-        for (int j = 1; j <= col; j++)
+        for (j = 1; j <= col; j++)
         {
-            cout << "*";
+            if (i == 1 || i == row)
+            {
+                cout << "*";
+            }
+            else if (j == 1 || j == col)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
         }
-        cout << "\n";
+        cout << '\n';
     }
 }
