0ce3e9197acb144c4970772dc1527ce0ddbde61c
aCrazyPOTATO <69080149+aCrazyPOTATO@users.noreply.github.com>
2022-04-27 07:30:30 +0545

    first from git cmd

--- tocheckprime.cpp
+++ tocheckprime.cpp
@@ -3,14 +3,20 @@ using namespace std;
 int main()
 {
     int a, b, c;
+    bool test = 0;
     cout << "Enter the number to check  wheather its prime or not:" << endl;
     cin >> a;
-    if (a / 2 == 0)
+    for (b = 2; b < a; b++)
     {
-        cout << "Its  Prime number";
+        if (a % b == 0)
+        {
+            cout << "Its not Prime number";
+            test = 1;
+            break;
+        }
     }
-    else
+    if (test == 0)
     {
-        cout << "Its not prime number!";
+        cout << "Its Prime";
     }
 }
